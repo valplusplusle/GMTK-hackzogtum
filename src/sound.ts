135129bf784612a31c1e922253e76eb5a23cdb44
Tobias Wich <tobias.wich@ecsec.de>
2020-07-11 20:42:49 +0200

    A little bit work on the packaging side

--- src/sound.ts
+++ src/sound.ts
@@ -1,4 +1,9 @@
+// <reference path="./index" />
+
+//import bgSound from '../assets/audio/music/ersteIdee.wav';
+//import bgSound from './ersteIdee.wav';
+
 export function playBackgroundMusic() {
-	var backgroundSongV1 = new Audio('../assets/audio/music/ersteIdee.wav');
+	var backgroundSongV1 = new Audio('../assets/audio/music/Theme1.mp3');
     backgroundSongV1.play();
 }
--- webpack.config.js
+++ webpack.config.js
@@ -2,31 +2,38 @@ const path = require('path');
 module.exports = {
 	entry: './src/index.ts',
 	devtool: 'inline-source-map',
+	devServer: {
+		contentBase: './',
+		//publicPath: '/dist/',
+	},
 	module: {
-		rules: [{
-			test: /\.tsx?$/,
-			use: 'ts-loader',
-			exclude: /node_modules/
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: 'ts-loader',
+				exclude: /node_modules/
+			},
+			{
+				test: /\.css$/,
+				use: [
+					'style-loader',
+					'css-loader',
+				],
+			},
+			{
+				test: /\.(png|svg|jpg|gif)$/,
+				use: [
+					'file-loader',
+				],
 		},
-		{
-			test: /\.css$/,
-			use: [
-				'style-loader',
-				'css-loader',
-			],
+			{
+				test: /\.(wav|mp3)$/,
+				use: [
+					'file-loader',
+				],
 		},
-		{
-			test: /\.(png|svg|jpg|gif)$/,
-			use: [
-				'file-loader',
-			],
-		},
-		{
-			test: /\.html$/,
-			use: [
-				'html-loader',
-			],
-		}]
+
+		]
 	},
 	resolve: {
 		extensions: ['.ts', '.js', '.tsx']
