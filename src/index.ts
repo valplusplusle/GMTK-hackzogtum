6ff4efbd0fde417b7450c39018d44999b669ab84
legion <florian.c.otto@web.de>
2020-07-11 23:02:18 +0200

    Add function for registering callbacks in dom

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import {foo} from './keyProcessor';
 import {playBackgroundMusic} from './sound';
-import {spawnRandomViruses} from './virusSpawner';
+import {clickedVirus as clickedVirusInner, spawnRandomViruses} from './virusSpawner';
 
 import '../assets/style/style.css';
 import '../assets/style/key.css';
@@ -8,6 +8,20 @@ import '../assets/style/virus.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
+
+function registerCBs(){ 
+    for(var i=0; i<7; i++){
+        let div = document.getElementById(`virus${i}`);
+        if(div){
+            div.onclick = () => {
+                clickedVirusInner(i);
+                console.log("test");
+            };
+        }
+    }
+}
+
 playBackgroundMusic();
 foo();
 spawnRandomViruses();
+registerCBs();
