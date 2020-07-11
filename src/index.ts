53124a48e7ef7fcc855183040ea66b22009524fe
valplusplusle <v.kummert@drmnt.de>
2020-07-11 15:56:28 +0200

    add background music

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 import {foo} from './keyprocessor';
+import {loopBackgroundMusic} from './sound';
 
+playBackgroundMusic();
 foo();
---
+++ src/sound.ts
@@ -0,0 +1,4 @@
+export function playBackgroundMusic() {
+	var backgroundSongV1 = new Audio('../assets/audio/music/ersteIdee.wav');
+    backgroundSongV1.play();
+}
