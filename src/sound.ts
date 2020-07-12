05497136ee350a75acbc739e195293643969dc84
legion <florian.c.otto@web.de>
2020-07-12 11:12:16 +0200

    Check for fastSkip function since chrome is not supporting

--- src/sound.ts
+++ src/sound.ts
@@ -11,7 +11,9 @@ export function stopBackgroundMusic() {
 	console.log("stop bg music called.");
 	let audio = document.getElementById("bgaudio");
 	if (audio instanceof HTMLAudioElement) {
-		audio.fastSeek(0);
+		if(audio.fastSeek){
+			audio.fastSeek(0);
+		}
 		audio.pause();
 	}
 }
