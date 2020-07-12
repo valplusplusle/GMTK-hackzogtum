b66d6840618931db9399afa40fecf273c1871ec1
legion <florian.c.otto@web.de>
2020-07-12 13:44:51 +0200

    Longer time more percoent to solve

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 
 //duration for each challenges in ms
-export const TIME_WINDOW : number = 5000; 
+export const TIME_WINDOW : number = 10000; 
 export const GAME_TICK_DURATION : number = 50;
 
 //min and max for target of action SPAM
@@ -15,7 +15,7 @@ export const MIN_TARGET_TIME : number = 4000;
 //export const AVAIL_KEYS = ["A", "B", "C", "D", "E", "F", "G", "H", "I"];
 export const AVAIL_KEYS = ["1","2","3","4","5","6","7","8","9","0","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
-export const PERCENT_OF_TO_SOLVE_CHALLENGS_TO_NOT_DIE :number = .3;
+export const PERCENT_OF_TO_SOLVE_CHALLENGS_TO_NOT_DIE :number = .8;
 
 //if difficulty exceeds the thresh the gettin harder callback is called
 export const DIFFICULTY_INCREASE = .3
