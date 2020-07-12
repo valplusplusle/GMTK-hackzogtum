6da9c870cc5f524a4420d6b39331400c268b582f
legion <florian.c.otto@web.de>
2020-07-12 12:27:32 +0200

    Use all keys

--- src/config.ts
+++ src/config.ts
@@ -12,8 +12,8 @@ export const MAX_TARGET_TIME : number = 500;
 export const MIN_TARGET_TIME : number = 4000; 
 
 //available keys which are used to gen challenges
-export const AVAIL_KEYS = ["A", "B", "C", "D", "E", "F", "G", "H", "I"];
-
+//export const AVAIL_KEYS = ["A", "B", "C", "D", "E", "F", "G", "H", "I"];
+export const AVAIL_KEYS = ["1","2","3","4","5","6","7","8","9","0","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
 export const PERCENT_OF_TO_SOLVE_CHALLENGS_TO_NOT_DIE :number = .3;
 
