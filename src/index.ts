4a10b80d59050bfb5468cbc61094ce34d571e993
Tobias Wich <tobias.wich@ecsec.de>
2020-07-12 03:26:11 +0200

    Clean up virus file and put code in a class

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import {KeyGame} from './keyGame';
 import {playBackgroundMusic} from './sound';
-import {clickedVirus, spawnRandomViruses} from './virusSpawner';
+import {VirusController} from './virusSpawner';
 
 import '../assets/style/style.css';
 import '../assets/style/key.css';
@@ -8,13 +8,15 @@ import '../assets/style/virus.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
+let vc = new VirusController();
+
 
 function registerCBs(){ 
     for(var i=0; i<7; i++){
         let div = document.getElementById(`virus${i}`);
         if(div){
             div.onclick = () => {
-                clickedVirus(event);
+                vc.clickedVirus(event);
             };
         }
 	}
@@ -27,18 +29,21 @@ function registerCBs(){
 			if (splash) {
 				splash.style.display = "none";
 			}
+			startGame();
 		});
 	}
 }
 
-window.localStorage.setItem('points', '0'); // init game with 0 Points
-playBackgroundMusic();
+function startGame() {
+	const keygame = new KeyGame();
+	keygame.registerEndOfGameCB(()=>{console.log("you fucked up")}); 
+	keygame.registerGettinCriticalCB(()=>{console.log("it's gettin harder")}); 
+	
+	keygame.startKeyGame();
+	
+	vc.start();
+}
 
-const keygame = new KeyGame();
-keygame.registerEndOfGameCB(()=>{console.log("you fucked up")}); 
-keygame.registerGettinCriticalCB(()=>{console.log("it's gettin harder")}); 
+//playBackgroundMusic();
 
-keygame.startKeyGame();
-
-spawnRandomViruses();
 registerCBs();
--- src/virusSpawner.ts
+++ src/virusSpawner.ts
@@ -6,49 +6,55 @@ function randomIntFromInterval(min: number, max: number) { // min and max includ
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-export function spawnRandomViruses() {
-    let virusScreen = []
-    for(let i = 0; i < 7; i++) {
-        var random_boolean = Math.random() >= 0.5;
-        virusScreen.push(random_boolean);
-    }
-    window.localStorage.setItem('virusList',  JSON.stringify(virusScreen));
-    drawViruses();
-}
 
-export function drawViruses() {
-    let virusScreen = JSON.parse(window.localStorage.getItem('virusList')!);
-    virusScreen.forEach((virus:string, index:number) => {
-        if(virus) {
-            document.getElementById("virus"+index)?.classList.remove("hidden");
-            document.getElementById("virus"+index)?.classList.add("shown");  
-        } else {
-            document.getElementById("virus"+index)?.classList.remove("shown");
-            document.getElementById("virus"+index)?.classList.add("hidden");  
+export class VirusController {
+    private virusScreen: Array<boolean> = [];
+    private points = 0;
+
+    constructor() {
+    }
+
+    public start() {
+        this.spawnRandomViruses();
+    }
+    
+    public spawnRandomViruses() {
+        this.virusScreen = [];
+        for(let i = 0; i < 7; i++) {
+            var random_boolean = Math.random() >= 0.5;
+            this.virusScreen.push(random_boolean);
         }
-    });
-}
-
-export async function clickedVirus(event:any) {
-    addPoints();
-
-
-    let virusId = event.target.id.slice(-1);
-    let virusScreen = JSON.parse(window.localStorage.getItem('virusList')!);
-    virusScreen[virusId] = false;
-    window.localStorage.setItem('virusList',  JSON.stringify(virusScreen));
-    drawViruses();
-    if(virusScreen.every((element: any) => !element)) {
-        await sleep(randomIntFromInterval(1000,4000));
-        spawnRandomViruses();
-        drawViruses();
+        this.drawViruses();
     }
-}
 
-export function addPoints() {
-    let points = window.localStorage.getItem('points')!;
-    let pointsAsNumber = Number(points);
-    pointsAsNumber = pointsAsNumber + 10;
-    window.localStorage.setItem('points', pointsAsNumber.toString());
-    document.getElementById('screenScore')!.innerHTML = pointsAsNumber.toString();
+    public drawViruses() {
+        this.virusScreen.forEach((virus:boolean, index:number) => {
+            if(virus) {
+                document.getElementById("virus"+index)?.classList.remove("hidden");
+                document.getElementById("virus"+index)?.classList.add("shown");  
+            } else {
+                document.getElementById("virus"+index)?.classList.remove("shown");
+                document.getElementById("virus"+index)?.classList.add("hidden");  
+            }
+        });
+    }
+
+    public async clickedVirus(event:any) {
+        this.addPoints();
+    
+        let virusId = event.target.id.slice(-1);
+        this.virusScreen[virusId] = false;
+        this.drawViruses();
+        if(this.virusScreen.every((element: any) => !element)) {
+            await sleep(randomIntFromInterval(1000,4000));
+            this.spawnRandomViruses();
+            this.drawViruses();
+        }
+    }
+    
+    public addPoints() {
+        this.points += 10;
+        document.getElementById('screenScore')!.innerHTML = this.points.toString();
+    }
+
 }
