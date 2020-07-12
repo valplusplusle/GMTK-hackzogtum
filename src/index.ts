ff6fc14495027fbde8ea2c403515229921a8d0f2
legion <florian.c.otto@web.de>
2020-07-12 03:43:08 +0200

    Fix starting game

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,14 @@ import '../assets/style/virus.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
+const keygame = new KeyGame();
+keygame.registerEndOfGameCB(()=>{
+    console.log("you fucked up");
+    alert("you fucked up - the system is out of control ;)")
+}); 
+keygame.registerGettinCriticalCB(()=>{console.log("it's gettin harder")}); 
+
+
 
 function registerCBs(){ 
     for(var i=0; i<7; i++){
@@ -23,6 +31,8 @@ function registerCBs(){
 	if (startGameBtn) {
 		startGameBtn.onclick = (e => {
 			playBackgroundMusic();
+            keygame.startKeyGame();
+            spawnRandomViruses();
 			let splash = document.getElementById("splash");
 			if (splash) {
 				splash.style.display = "none";
@@ -33,11 +43,5 @@ function registerCBs(){
 
 window.localStorage.setItem('points', '0'); // init game with 0 Points
 registerCBs();
-playBackgroundMusic();
 
-const keygame = new KeyGame();
-keygame.registerEndOfGameCB(()=>{console.log("you fucked up"); alert("you fucked up")}); 
-keygame.registerGettinCriticalCB(()=>{console.log("it's gettin harder")}); 
-keygame.startKeyGame();
 
-spawnRandomViruses();
